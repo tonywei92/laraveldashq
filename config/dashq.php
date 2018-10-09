5de4c78580b6531882d4227d38a3f294d6e46a92
Tony Song <tonywei92@gmail.com>
2018-10-09 11:46:55 +0700

    fix readme

--- config/dashq.php
+++ config/dashq.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    //
+    'middleware' => [],
+    'uri' => env('DASHQ_URI', 'dashq')
 ];
--- src/routes.php
+++ src/routes.php
@@ -1,23 +1,31 @@
 <?php
-Route::middleware(['web'])->group(function(){
-    Route::get('dashq', 'TonySong\DashQ\controllers\Controller@index')->name('dashq::home');
-    Route::get('dashq/jobs', 'TonySong\DashQ\controllers\Controller@jobs')->name('dashq::jobs');
-    Route::get('dashq/failedjobs', 'TonySong\DashQ\controllers\Controller@failedJobs')->name('dashq::failedjobs');
+$dashqMiddleware = [];
+$dashqConfig = config('dashq');
+$dashqUri = $dashqConfig['uri'];
+
+if(is_array($dashqConfig['middleware'])){
+    $dashqMiddleware = array_merge($dashqMiddleware,$dashqConfig['middleware']);
+}
+$dashqMiddleware[] = 'web';
+
+
+Route::group(['middleware' => $dashqMiddleware, 'prefix' => $dashqUri], function(){
+    Route::get('/', 'TonySong\DashQ\controllers\Controller@index')->name('dashq::home');
+    Route::get('jobs', 'TonySong\DashQ\controllers\Controller@jobs')->name('dashq::jobs');
+    Route::get('failedjobs', 'TonySong\DashQ\controllers\Controller@failedJobs')->name('dashq::failedjobs');
     //action
     //on failed jobs page
-    Route::get('dashq/retryjob/{id}', 'TonySong\DashQ\controllers\Controller@retryFailedJob')->name('dashq::retryFailedJob');
-    Route::post('dashq/retryfailedjobs', 'TonySong\DashQ\controllers\Controller@retryFailedJobs')->name('dashq::retryFailedJobs');
-    Route::get('dashq/retryalljobs', 'TonySong\DashQ\controllers\Controller@retryAllFailedJobs')->name('dashq::retryAllFailedJobs');
-    Route::get('dashq/deletefailedjob/{id}', 'TonySong\DashQ\controllers\Controller@deleteFailedJob')->name('dashq::deleteFailedJob');
-    Route::post('dashq/deletefailedjobs', 'TonySong\DashQ\controllers\Controller@deleteFailedJobs')->name('dashq::deleteFailedJobs');
-    Route::get('dashq/deleteallfailedjobs', 'TonySong\DashQ\controllers\Controller@deleteAllFailedJobs')->name('dashq::deleteAllFailedJobs');
+    Route::get('retryjob/{id}', 'TonySong\DashQ\controllers\Controller@retryFailedJob')->name('dashq::retryFailedJob');
+    Route::post('retryfailedjobs', 'TonySong\DashQ\controllers\Controller@retryFailedJobs')->name('dashq::retryFailedJobs');
+    Route::get('retryalljobs', 'TonySong\DashQ\controllers\Controller@retryAllFailedJobs')->name('dashq::retryAllFailedJobs');
+    Route::get('deletefailedjob/{id}', 'TonySong\DashQ\controllers\Controller@deleteFailedJob')->name('dashq::deleteFailedJob');
+    Route::post('deletefailedjobs', 'TonySong\DashQ\controllers\Controller@deleteFailedJobs')->name('dashq::deleteFailedJobs');
+    Route::get('deleteallfailedjobs', 'TonySong\DashQ\controllers\Controller@deleteAllFailedJobs')->name('dashq::deleteAllFailedJobs');
 
     //on jobs
-    Route::get('dashq/deletejob/{id}', 'TonySong\DashQ\controllers\Controller@deleteJob')->name('dashq::deleteJob');
-    Route::post('dashq/deletejobs', 'TonySong\DashQ\controllers\Controller@deleteJobs')->name('dashq::deleteJobs');
-    Route::get('dashq/deletealljob', 'TonySong\DashQ\controllers\Controller@deleteAllJobs')->name('dashq::deleteAllJobs');
-
-
+    Route::get('deletejob/{id}', 'TonySong\DashQ\controllers\Controller@deleteJob')->name('dashq::deleteJob');
+    Route::post('deletejobs', 'TonySong\DashQ\controllers\Controller@deleteJobs')->name('dashq::deleteJobs');
+    Route::get('deletealljob', 'TonySong\DashQ\controllers\Controller@deleteAllJobs')->name('dashq::deleteAllJobs');
 
 });
 
